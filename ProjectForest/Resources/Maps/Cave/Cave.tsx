74f418a9ec0f5e69b4abf34f7a00d58620e2b8b8
= <=>
2025-09-15 16:15:57 -0400

    more tiles.

--- ProjectForest/Resources/Maps/Cave/Cave.tsx
+++ ProjectForest/Resources/Maps/Cave/Cave.tsx
@@ -2,7 +2,7 @@
 <tileset version="1.10" tiledversion="1.11.2" name="Cave" tilewidth="8" tileheight="8" tilecount="4096" columns="64">
  <image source="../../Sprites/Tilesets/Cave.png" width="512" height="512"/>
  <wangsets>
-  <wangset name="RockBlock" type="corner" tile="-1">
+  <wangset name="StoneBlock" type="corner" tile="-1">
    <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
    <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
    <wangtile tileid="0" wangid="0,0,0,1,0,0,0,0"/>
@@ -58,7 +58,7 @@
    <wangtile tileid="324" wangid="0,1,0,0,0,0,0,1"/>
    <wangtile tileid="325" wangid="0,0,0,0,0,0,0,1"/>
   </wangset>
-  <wangset name="RockPlatform" type="edge" tile="-1">
+  <wangset name="StonePlatform" type="edge" tile="-1">
    <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
    <wangtile tileid="384" wangid="0,0,1,0,0,0,0,0"/>
    <wangtile tileid="385" wangid="0,0,1,0,0,0,1,0"/>
@@ -72,6 +72,77 @@
    <wangtile tileid="640" wangid="1,0,0,0,1,0,0,0"/>
    <wangtile tileid="704" wangid="1,0,0,0,1,0,0,0"/>
    <wangtile tileid="768" wangid="1,0,0,0,0,0,0,0"/>
+  </wangset>
+  <wangset name="SmoothStoneBlock" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="11" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="12" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="13" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="14" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="15" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="16" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="17" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="18" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="19" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="20" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="75" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="76" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="77" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="78" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="79" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="80" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="81" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="82" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="83" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="84" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="139" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="140" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="141" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="142" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="143" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="144" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="145" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="146" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="147" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="148" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="203" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="204" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="205" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="206" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="207" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="208" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="209" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="210" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="211" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="212" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="267" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="268" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="269" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="270" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="271" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="272" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="331" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="332" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="333" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="334" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="335" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="336" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+  <wangset name="SmoothStonePlatform" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="395" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="396" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="397" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="398" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="399" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="400" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="459" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="523" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="587" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="651" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="715" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="779" wangid="1,0,0,0,0,0,0,0"/>
   </wangset>
  </wangsets>
 </tileset>
